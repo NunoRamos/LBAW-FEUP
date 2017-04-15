c8fdac10906185269ad062c03d0e72853936b112
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-04-15 12:08:27 +0100

    Updated init.php.

--- src/config/init.php
+++ src/config/init.php
@@ -4,10 +4,10 @@ session_start();
 
 error_reporting(E_ERROR | E_WARNING); // E_NOTICE by default
 
-$BASE_DIR = '/home/nuno/Documents/GitHub/LBAW-FEUP/src/';
+$BASE_DIR = '/home/nuno/Documents/GitHub/LBAW-FEUP/src/'; //FIXME:
 $BASE_URL = '/';
 
-$conn = new PDO('pgsql:host=dbm.fe.up.pt;dbname=lbaw1612', 'lbaw1612', 'ls84wu43');
+$conn = new PDO('pgsql:host=dbm.fe.up.pt;dbname=lbaw1612', 'lbaw1612', 'PASSWORD'); //FIXME:
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
