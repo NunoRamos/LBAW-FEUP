38d64550849b28b82a08467548dc0de3f2649a7e
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-04-15 12:04:23 +0100

    Updated gitignore.

--- src/config/init.php
+++ src/config/init.php
@@ -7,7 +7,7 @@ error_reporting(E_ERROR | E_WARNING); // E_NOTICE by default
 $BASE_DIR = '/home/nuno/Documents/GitHub/LBAW-FEUP/src/';
 $BASE_URL = '/';
 
-$conn = new PDO('pgsql:host=dbm.fe.up.pt;dbname=lbaw1612', 'lbaw1612', 'password');
+$conn = new PDO('pgsql:host=dbm.fe.up.pt;dbname=lbaw1612', 'lbaw1612', 'ls84wu43');
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
