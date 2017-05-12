dd2760dd517684b2556efd586a6edf0fc792a7ec
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-05-12 10:15:17 +0100

    Fixed landing page.

--- src/pages/misc/landing_page.php
+++ src/pages/misc/landing_page.php
@@ -3,6 +3,6 @@ include_once '../../config/init.php';
 include_once '../../database/content.php';
 include_once '../../database/users.php';
 
-$smarty->assign('reply', getMostRecentQuestions(10));
+$smarty->assign('questions', getMostRecentQuestions(10));
 $smarty->assign('tags', getAllTags());
 $smarty->display('misc/landing_page.tpl');
