04c493e179d035823d52067244366cf1bee86a6c
VascoRibeiro96 <vascovsc1996@gmail.com>
2017-05-15 17:26:08 +0100

    Profile Page
    Almost done, missing css

--- src/pages/users/profile_page.php
+++ src/pages/users/profile_page.php
@@ -1,5 +1,6 @@
 <?php
 include_once '../../config/init.php';
 include_once($BASE_DIR .'database/users.php');
+include_once($BASE_DIR .'database/content.php');
 
 $smarty->display('users/profile_page.tpl');
