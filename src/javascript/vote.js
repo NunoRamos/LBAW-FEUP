448748197e631596b4a6ea8ca28f6c051a070a69
Nuno Ramos <nunor07.unfc@gmail.com>
2017-05-20 22:42:22 +0100

    Updating rating

---
+++ src/javascript/vote.js
@@ -0,0 +1,93 @@
+const UP = 1;
+const DOWN= 0;
+const REMOVE_VOTE = -1;
+
+let id;
+let newVote;
+let oldVote;
+
+function addPositiveVote(userId,contentId){
+    newVote = UP;
+    vote(userId,contentId);
+}
+
+function addNegativeVote(userId,contentId){
+    newVote = DOWN;
+    vote(userId,contentId);
+}
+
+function vote(userId,contentId){
+
+    if(userId == 0)
+        return;
+
+    id = contentId;
+
+    oldVote = parseInt($('div[data-content-id=' + id + ']').attr("data-vote-positive"));
+
+    let vote;
+
+    if(oldVote == newVote)
+        vote = REMOVE_VOTE;
+    else
+        vote = newVote;
+
+    $('div[data-content-id=' + id + ']').attr("data-vote-positive", vote);
+
+    $.ajax({
+        method: "GET",
+        url: "../../api/add_vote.php",
+        data: {
+            contentId: contentId,
+            vote: vote,
+            userId: userId,
+        }
+    }).done(changeVoteColor);
+}
+
+function changeVoteColor(){
+
+    let selector = 'div[data-content-id=' + id + ']';
+    let rating = parseInt($(selector).find('div.rating span').text());
+
+    console.log(rating);
+
+    switch (newVote){
+        case UP:
+            if(oldVote == UP){
+                $(selector).find('div.positive span').toggleClass("positive-vote");
+                rating--;
+            }
+            else if(oldVote == DOWN){
+                $(selector).find('div.positive span').toggleClass("positive-vote");
+                $(selector).find('div.negative span').toggleClass("negative-vote");
+                rating += 2;
+            }
+            else {
+                $(selector).find('div.positive span').toggleClass("positive-vote");
+                rating++;
+            }
+            break;
+        case DOWN:
+            if(oldVote == DOWN){
+                $(selector).find('div.negative span').toggleClass("negative-vote");
+                rating++;
+            }
+            else if(oldVote == UP){
+                $(selector).find('div.positive span').toggleClass("positive-vote");
+                $(selector).find('div.negative span').toggleClass("negative-vote");
+                rating -= 2;
+            }
+            else {
+                $(selector).find('div.negative span').toggleClass("negative-vote");
+                rating--;
+            }
+            break;
+        default:
+            break;
+    }
+
+    //Updating rating
+    $(selector).find('div.rating span').text(rating);
+}
+
--- src/templates/content/common/rating.tpl
+++ src/templates/content/common/rating.tpl
@@ -1,10 +1,15 @@
+{if !isset($USERID)}
+    {assign "userId" "0"}
+{else}
+    {assign "userId" $USERID}
+{/if}
 <div data-content-id="{$content["id"]}" data-vote-positive="{if $content["positive"] === FALSE}0{elseif $content["positive"] === TRUE}1{else}-1{/if}">
-    <div class="text-center anchor clickable positive" onclick="addPositiveVote({$USERID},{$content["id"]})">
+    <div class="text-center anchor clickable positive" onclick="addPositiveVote({$userId},{$content["id"]})">
         <span class="glyphicon glyphicon-triangle-top {if $content["positive"] === TRUE}positive-vote{/if}" aria-hidden="true"></span>
     </div>
     <div class="text-center rating"><span>{$content["rating"]}</span></div>
 
-    <div class="text-center anchor clickable negative" onclick="addNegativeVote({$USERID},{$content["id"]})">
+    <div class="text-center anchor clickable negative" onclick="addNegativeVote({$userId},{$content["id"]})">
         <span class="glyphicon glyphicon-triangle-bottom {if $content["positive"] === FALSE}negative-vote{/if}" aria-hidden="true"></span>
 
     </div>
