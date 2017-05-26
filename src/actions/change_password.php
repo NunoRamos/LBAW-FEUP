02c92191297b686601c366201a08513913c845a6
joaogomes04 <up201403275@fe.up.pt>
2017-05-26 16:52:11 +0100

    upload img done! needs to center buttons

--- src/actions/change_password.php
+++ src/actions/change_password.php
@@ -8,14 +8,19 @@ $userId = $smarty->getTemplateVars('USERID');
 
 $currPassword = htmlspecialchars($_POST['curr-password']);
 $newPassword = htmlspecialchars($_POST['new-password']);
-$repeatPassword = htmlspecialchars($_POST['repeat-password']);
 
-
+$smarty->assign('ERROR_PASSOWORD', '-1');
+header('Location: ' . $smarty->getTemplateVars('BASE_URL')  . 'src/pages/users/settings_page.php');
+if (!checkCurrentPassword($userId,$newPassword))
 if(checkCurrentPassword($userId,$currPassword)) {
     changePassword($userId, $newPassword);
     header('Location: ' . $smarty->getTemplateVars('BASE_URL')  . 'src/pages/users/profile_page.php');
 }else
     header('Location: ' . $smarty->getTemplateVars('BASE_URL')  . 'src/pages/users/settings_page.php');
+else{
+
+}
+
 
 
 
